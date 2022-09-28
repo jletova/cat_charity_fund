f3e2610883fac2c2ea91dfff2c121c952fd60ec5
Julia Letova <julialikh@gmail.com>
2022-09-28 17:59:25 +0200

    Changed base settings

--- app/core/config.py
+++ app/core/config.py
@@ -4,10 +4,10 @@ from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str
-    app_description: str
+    app_title: str = 'QRKot'
+    app_description: str = 'Приложение для Благотворительного фонда поддержки котиков'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
-    app_author: str
+    app_author: str = '@jletova'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
